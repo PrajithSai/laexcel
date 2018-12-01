587be1413cf8ca38016c7c2d8a93d61075e6b124
Prajith Sai <Prajith@Prajiths-MacBook-Air.local>
2018-12-02 00:25:04 +0530

    Fixed nam to name

--- src/api/campus/controller.js
+++ src/api/campus/controller.js
@@ -8,7 +8,7 @@ const formatData = campus => {
     city: campus.city._id,
     cityName: campus.city.cityName,
     parentBranch: campus.parentBranch._id,
-    branchName: campus.parentBranch.nam
+    branchName: campus.parentBranch.name
   }
 }
 
